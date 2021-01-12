eeef13fcc0e5b07d317d5f39933ce78a3a52242b
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-01-12 22:25:46 +0530

    I am rich converted to i am poor

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 // starting point of our flutter application
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Center(
-              child: Text("I am Rich AF"),
-            ),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          backgroundColor: Colors.blueGrey,
-          body: Center(
-            child: Image(
-              image: AssetImage(
-                'img/diamond.png'
-              ),
-            ),
-          ),
-        ),
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.green,
+      title: Center(
+        child: Text("I am Poor"),
       ),
-    );
+    ),
+    body: Center(
+      child: Image(
+        image: AssetImage('img/icons8_british_pound_24px.png'),
+        width: 400000,
+        height: 400000,
+      ),
+    ),
+  ),
+));
